7aa53b86cbf4c1f18ec2c6fbe30159b84652aae4
Kirubakaran Karunakaran <kkarun19@in.ibm.com>
2020-03-09 15:34:06 +0530

    second

---
+++ calc.py
@@ -0,0 +1,17 @@
+def add (x,y)
+ *** Add Function***
+ return x+y
+ 
+def Subtract (x,y)
+ *** Subtract Function***
+ return x-y 
+ 
+ 
+ def multiply (x,y)
+ *** multiply Function***
+ return x*y
+ 
+def divide (x,y)
+ *** divide Function***
+ return x/y 
+ 
